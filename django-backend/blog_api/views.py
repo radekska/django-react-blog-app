d7f14a8e43f4abccc8b54ea2d3147173d82b5335
radekska <radoslaw.skalbania@gmail.com>
2021-10-08 12:44:43 +0200

    implement get_object method to retrieve blogs both by slug and id.

--- django-backend/blog_api/views.py
+++ django-backend/blog_api/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from blog.models import Post
 from django.db.models import QuerySet
 from rest_framework import status, viewsets
@@ -19,46 +21,54 @@ class PostUserWritePermission(BasePermission):
         return request.user == obj.author
 
 
-class PostView(viewsets.ViewSet):
-    permission_classes = [IsAuthenticated]
+class PostView(viewsets.ModelViewSet):
+    permission_classes = [PostUserWritePermission]
+    serializer_class = PostSerializer
 
     @property
     def queryset(self) -> QuerySet:
         return Post.post_objects.all()
 
-    def list(self, request: Request) -> Response:
-        serializer = PostSerializer(self.queryset, many=True)
-        return Response(serializer.data)
+    def get_object(self, queryset: QuerySet = None, **kwargs: Any) -> Post:
+        item = self.kwargs.get("pk")
+        try:
+            item = int(item)
+        except ValueError:
+            return get_object_or_404(Post, slug=item)
+        return get_object_or_404(Post, id=item)
 
-    def retrieve(self, request: Request, pk: int = None) -> Response:
-        post = get_object_or_404(self.queryset, pk=pk)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
-
-    @staticmethod
-    def create(request: Request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            post = Post.objects.create(**serializer.validated_data)
-            post.save()
-            return Response(status=status.HTTP_201_CREATED)
-        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def update(self, request: Request, pk: int = None):
-        pass
-
-    def partial_update(self, request: Request, pk: int = None):
-        pass
-
-
-def destroy(self, request, pk=None):
-    pass
-
-# class PostList(generics.ListCreateAPIView):
-#     serializer_class = PostSerializer
+# ViewSet implementation for PostView #
+# The base ViewSet class does not provide any actions by default.
+# class PostView(viewsets.ViewSet):
+#     permission_classes = [IsAuthenticated]
 #
+#     @property
+#     def queryset(self) -> QuerySet:
+#         return Post.post_objects.all()
 #
-# class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-#     permission_classes = [IsAuthenticated, PostUserWritePermission]
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
+#     def list(self, request: Request) -> Response:
+#         serializer = PostSerializer(self.queryset, many=True)
+#         return Response(serializer.data)
+#
+#     def retrieve(self, request: Request, pk: int = None) -> Response:
+#         post = get_object_or_404(self.queryset, pk=pk)
+#         serializer = PostSerializer(post)
+#         return Response(serializer.data)
+#
+#     @staticmethod
+#     def create(request: Request):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             post = Post.objects.create(**serializer.validated_data)
+#             post.save()
+#             return Response(status=status.HTTP_201_CREATED)
+#         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def update(self, request: Request, pk: int = None):
+#         pass
+#
+#     def partial_update(self, request: Request, pk: int = None):
+#         pass
+#
+#     def destroy(self, request, pk=None):
+#         pass
