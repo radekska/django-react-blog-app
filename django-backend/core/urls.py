c0c93fa5956d688f8a58098508540e85c11ccd97
radekska <radoslaw.skalbania@gmail.com>
2021-10-08 12:44:51 +0200

    remove staticfiles_urlpatterns

--- django-backend/core/urls.py
+++ django-backend/core/urls.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
