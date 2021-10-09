368d09fb13875c34e24fe27d16ef896e1328dbf6
radekska <radoslaw.skalbania@gmail.com>
2021-10-09 21:08:26 +0200

    add core api schema

--- django-backend/core/settings.py
+++ django-backend/core/settings.py
@@ -131,7 +131,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
 # Project Based Permissions - Names speak for them selves:
 # AllowAny
