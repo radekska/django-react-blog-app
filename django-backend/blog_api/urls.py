bafc6582dab2af0288dcd421f078c1d044904921
radekska <radoslaw.skalbania@gmail.com>
2021-10-10 20:38:56 +0200

    add admin panel endpoints to django backend.

--- django-backend/blog_api/urls.py
+++ django-backend/blog_api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from blog_api.views import PostView, PostListDetailFilter
+from blog_api.views import AdminPostViewSet, PostViewSet, PostListDetailFilter
 
 app_name = "blog_api"
 router = DefaultRouter()
-router.register("posts", PostView, basename="post")
+router.register("posts", PostViewSet, basename="post")
+router.register("admin", AdminPostViewSet, basename="adminpost")
 urlpatterns = [
     path("search/", PostListDetailFilter.as_view(), name="postsearch")
 ]
