c7187fe37916cdfaedc27448bffa7496c35dea91
radekska <radoslaw.skalbania@gmail.com>
2021-10-24 13:53:29 +0200

    add image field to a PostSerializer.

--- django-backend/blog_api/serializers.py
+++ django-backend/blog_api/serializers.py
@@ -5,8 +5,8 @@ from blog.models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('id', 'category', 'title', 'slug', 'author',
-                  'excerpt', 'content', 'status')
+        fields = ("id", "category", "title", "image", "slug", "author",
+                  "excerpt", "content", "status")
         read_only_fields = ("published", "status")
         extra_fields = {"published": {"required": False}}
         validators = []  # Remove a default "unique together" constraint.
