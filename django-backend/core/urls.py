641cbb67a0692874f6341b0386942b65b57d284d
radekska <radoslaw.skalbania@gmail.com>
2021-10-24 12:33:04 +0200

    docs/ -> api/docs, schema/ -> api/schema

--- django-backend/core/urls.py
+++ django-backend/core/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('docs/', include_docs_urls(title="BlogAPI")),
-    path('schema', get_schema_view(
+    path('api/docs/', include_docs_urls(title="BlogAPI")),
+    path('api/schema', get_schema_view(
         title="BlogAPI",
         description="API for the BlogAPI",
         version="1.0.0"), name='blog_api-schema')
